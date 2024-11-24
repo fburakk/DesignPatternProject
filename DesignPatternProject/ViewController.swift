b7ba80fe63b7952dea1c2f7ddf42932268dfef6d
Burak Köse <iosdeveloper19@hotmail.com>
2024-11-24 20:44:23 +0300

    Initial Commit

---
+++ DesignPatternProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPatternProject
+//
+//  Created by Burak Köse on 24.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DesignPatternProjectTests/DesignPatternProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternProjectTests.swift
+//  DesignPatternProjectTests
+//
+//  Created by Burak Köse on 24.11.2024.
+//
+
+import Testing
+@testable import DesignPatternProject
+
+struct DesignPatternProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
